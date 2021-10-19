da774bee3a4fbb15f89d56b83910d9917f81e6ca
IliaIvanob <89130092+IliaIvanob@users.noreply.github.com>
2021-10-19 17:35:13 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#app').vide({
+    mp4: 'video/video-down/video.mp4',
+    webm: 'video/video-down/video.webm',
+    ogv: 'video/video-down/video.ogv'
+    });
+}) 
